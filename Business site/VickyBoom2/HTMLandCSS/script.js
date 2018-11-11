c3a6669a2ab60b87d1cbb855ec63d10447edd603
Slava Gergel <svyatoslav.gergel@ukr.net>
2018-11-11 12:14:59 +0200

    Very interesting project

---
+++ Business site/VickyBoom2/HTMLandCSS/script.js
@@ -0,0 +1,8 @@
+menu.onclick = function myFunction() {
+    var x = document.getElementById('topNav');
+    if (x.className === "topNav"){
+        x.className += " responsive"
+    }else{
+       x.className = "topNav";
+     }
+}
